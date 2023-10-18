d3ed0b181b485e5e2ec4201333e17a3f2329a6d8
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-10-18 23:57:20 +0530

    added files

---
+++ logggger/formatTime.js
@@ -0,0 +1,15 @@
+const chalk = require("chalk");
+
+const formatTime = () => {
+  const date = new Date();
+  const hour = date.getHours();
+  const min = date.getMinutes();
+  const sec = date.getSeconds();
+  const day = date.getDate();
+  const mon = date.getMonth();
+  const year = date.getFullYear();
+  const time = `[${day}-${mon}-${year} | ${hour}:${min}:${sec}]`;
+  return time;
+};
+
+module.exports = formatTime;
